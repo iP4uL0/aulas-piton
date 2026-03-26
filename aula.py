b92965b2cc9c7af317ea40039380e34e217826cf
Paulo Santos <160270216+iP4uL0@users.noreply.github.com>
2026-03-25 21:54:08 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,28 @@
+
+#Exercicio 1
+#PrecoOriginal = float(input("qual o valor original do produto?: "))
+#Desconto = PrecoOriginal * 0.15
+#precofinal = PrecoOriginal - Desconto
+#print("Preco com desconto é ", precofinal)
+
+#Exercicio 2
+#horario = int(input("Qual o horario que vc necessita?: "))
+#minutos = horario*60
+#print(horario,"horas são", minutos,"minutos")
+
+#Exercicio 3
+#Pessoas = int(input("Quantas pessoas são?: "))
+#Valor = float(input("Qual o valor da conta?: "))
+#divisao = Valor/Pessoas
+#print("O valor por pessoa é",divisao)
+
+
+#Exercicio 4
+
+Quadrado = float(input("tamanho do safado: "))
+
+Area = Quadrado*Quadrado
+
+perimetro = 4 * Quadrado
+
+print("A área do quadrado é:", Area,"e o perimetro é",perimetro)
